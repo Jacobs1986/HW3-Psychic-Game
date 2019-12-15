22f4e904963537a7096271981ce56266d2d78172
Sean Jacobson <sean.jacobson86@gmail.com>
2019-12-15 08:57:52 -0700

    Created an array for some of the words that will be used in the game, may change them later. Did a console.log test to make sure that the program can read them alright.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,21 @@ This is going to be a word game where the user can guess a random word from an a
 I need to create a list and a number generator that would randomly pick a word from the list.
 */
 
+/*
 //Test word
 let word = "Programming";
 
 // let's use .length to find the length of the string.
 console.log(`The word is ${word} and the length is ${word.length}.`);
+*/
+
+//Let's create the array that I will need for this game.
+let wordChoice = [
+    "Programmer",
+    "Bootcamp",
+    "Cookie",
+    "Canine"
+];
+
+// Let's just check the length of the array
+console.log(wordChoice[3]);
