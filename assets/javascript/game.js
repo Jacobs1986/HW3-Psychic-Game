3e3fc4d080dd28ce2999a575c90bf6a56abb62aa
Sean Jacobson <sean.jacobson86@gmail.com>
2019-12-15 13:07:50 -0700

    Tried a few different versions of the search. My concern is that when the search is done it still returns a -1. I'm worried that might have a negative effect in the future.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,21 +22,19 @@ let wordChoice = [
 
 //I am going to need a random number generator for this game for numbers between 0 - 4.
 let wordNumber = Math.floor(Math.random() * 4);
+selectedWord = wordChoice[wordNumber].toLowerCase();
+console.log(`The chosen word is ${selectedWord}`)
 
 // So far the code is working. Now I need to capture the key events when the player uses the keyboard.
 //I'm going to try adding the loop into the onkeyup function
+//The loop worked. Now I'm going to add the selectedWord into the loop search
 document.onkeyup = function(event) {
     letter = event.key;
     console.log(`You pressed the letter: ${letter}`);
-    test = "canine"
-    for (let i = 0; i < test.length; i++) {
-        let letterSearch = test.indexOf(letter, i);
-        if (letterSearch == -1) {
-            return console.log("The letter was not found.")
-            ;
-        } else {
-            console.log(`The letter was found at ${letterSearch}.`);
-        }
+    //search for the letter
+    for (let i = 0; i < selectedWord.length; i++) {
+       let letterSearch = selectedWord.indexOf(letter, i);
+       console.log(letterSearch);
     }
 }
 
