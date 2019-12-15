4e5e2e77382107e20e863a51470172b4487b0e2d
Sean Jacobson <sean.jacobson86@gmail.com>
2019-12-15 08:47:40 -0700

    Wrote some notes to get my thoughts about what the program needs to do. Started by creating a test word.

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+/*
+This is going to be a word game where the user can guess a random word from an array list.
+
+I need to create a list and a number generator that would randomly pick a word from the list.
+*/
+
+//Test word
+let word = "Programming";
+
+console.log(word);
