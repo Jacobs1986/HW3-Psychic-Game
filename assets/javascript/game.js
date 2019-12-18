a6c81d286e0333b9a4e7852061110d92df563d33
Sean Jacobson <sean.jacobson86@gmail.com>
2019-12-18 09:08:26 -0700

    I seem to have written a function that will break the interent. I'm going to consolidate my program a little bit more.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,12 +23,23 @@ let wordChoice = [
 //I am going to need a random number generator for this game for numbers between 0 - 4.
 let wordNumber = Math.floor(Math.random() * 4);
 selectedWord = wordChoice[wordNumber].toLowerCase();
-console.log(`The chosen word is ${selectedWord}`)
+console.log(`The chosen word is ${selectedWord}`);
 
 //Now I need to take the word and break it down into letters.
 wordSplitter = () => {
     letterArray = selectedWord.split("");
     //The letters will need to be put into an array that can be searched later.
-    return letterArray
+    return letterArray;
 }
 
+//I need a function that will search through the letterArray for the letter that the player guesses.
+findGuessedLetter = (letter) => {
+    let searchArray = letterArray;
+    let locations = [];
+    let searchFrom = -1;
+    while (searchArray.indexOf(letter, searchFrom + 1) > -1) {
+        searchFrom = searchArray.indexOf(letter, searchFrom);
+        locations.push(searchFrom);
+    }
+    return locations;
+}
