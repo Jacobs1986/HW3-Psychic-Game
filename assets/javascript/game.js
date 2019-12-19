f993383ad67e80acc02e1a26d332c461a13bdb31
Sean Jacobson <sean.jacobson86@gmail.com>
2019-12-19 08:24:38 -0700

    OK so I got the loop working FINALLY. It took some research but I created a while loop that will look for both instances of the guessedLetter, even if they are back to back.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,19 +23,25 @@ let wordChoice = [
 //I am going to need a random number generator for this game for numbers between 0 - 4.
 let wordNumber = Math.floor(Math.random() * 4);
 selectedWord = wordChoice[wordNumber].toLowerCase();
-//console.log(`The chosen word is ${selectedWord}`);
+console.log(`The chosen word is ${selectedWord}`);
 
-//Writing a test function to see if I can get it all to work right.
-function findGuessedLetter(testWord) {
-    for (i = 0; i < testWord.length; i++) {
-        let locations = [];
-        let searchFrom = -1;
-        while (testWord.indexOf("n", searchFrom + 1) > -1) {
-            searchFrom = testWord.indexOf("n", searchFrom);
-            locations.push(searchFrom);
-        }
-        return locations;
-        i++;
+//Writing a test function to see if I can get it all to work right. I am going to need two parameters for this.
+function findGuessedLetter(selectedWord, guessedLetter) {
+    //I am going to need a loop that will compare a gussed letter to a letter in the word.
+    //I'm thinking that maybe changing the word into an array might help.
+    let wordArray = selectedWord.split("");
+    console.log(wordArray.length);
+    //create an empty locations array
+    let locations = [];
+    //From there I would need to create a loop that will compare the guessed word to the index of a word in the array
+    //OK so I got the loop working, BUT i'm now having an issue when the same letter is back to back. 
+    let idx = wordArray.indexOf(guessedLetter);
+    while (idx != -1) {
+        locations.push(idx);
+        idx = wordArray.indexOf(guessedLetter, idx + 1);
     }
+
+    //If the word and guessWord match then push the location into the locations array
+    return locations
 }
 
